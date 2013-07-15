2f6dfb4e406b93a9b6bbc38df99f6ad78b26fb5f
MBaileyLove <mbaileylove@gmail.com>
2013-07-15 19:10:31 +0000

    Updated web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
+
+var fs= require("fs");
+
 var express = require('express');
+
+var fileContent= fs.readFileSync('index.html','utf8');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World!');
+  response.send(fileContent);
 });
 
 var port = process.env.PORT || 5000;
